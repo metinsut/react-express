28de0d2cf535ed6d3342320a20d5006de30f3625
metinsut <metin@gulenayva.com>
2018-05-25 14:19:39 +0300

    edited

--- src/containers/users/user.js
+++ src/containers/users/user.js
@@ -14,7 +14,6 @@ class User extends Component {
 
     render() {
         const user = this.props.user[0];
-        console.log("data", user);
         return (
             <section>
                 {user ? (
